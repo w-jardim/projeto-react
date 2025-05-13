cb4dfeb4f4880379b7371b607edcd6f6a2e9a501
w-jardim <wallacejardimmp@gmail.com>
2025-05-13 15:24:24 -0300

    testecommit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+
+
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+    
+      <header> <h1>ola mundo ...</h1>
+      <p>policial militar</p>
+      </header>
+
+      <main>
+        <h2>sobre mim</h2>
+        <p>Lorem ipsum dolor sit amet <strong> onsectetur </strong> </p>
+       <p>cadipisicing elit. 
+        Consectetur numquam, vel et maxime debitis odit quod ducimus magni 
+        magnam recusandae aliquid.</p> 
+        <p> Doloremque, sit? Repellat laudantium repellendus, 
+        labore itaque esse velit.</p>
+      </main>
+    <footer>
+&copy; wallace 2025
+    </footer>
+  
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
